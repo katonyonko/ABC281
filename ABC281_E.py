82a1c66e24f0ed85ef869d9f2b70c6d4abae1559
加藤真也 <katonyonko@yahoo.co.jp>
2022-12-11 21:56:47 +0900

    体調良ければFまでは解けて良さそう

--- ABC281_E.py
+++ ABC281_E.py
@@ -7,8 +7,8 @@ _INPUT = """\
 3 1 4 1 5 9
 10 6 3
 12 2 17 11 19 8 4 3 6 20
-4 3 2
-1 2 2 11
+20 4 2
+1 2 2 11 13 3 4 5 61 2 34 7 6 9 5 6 77 8 21 2
 """
 
 sys.stdin = io.StringIO(_INPUT)
@@ -20,10 +20,8 @@ for __ in range(case_no):
   tmp=sorted([(A[i],i) for i in range(M)])
   ans=sum([tmp[i][0] for i in range(K)])
   h,h2=[],[]
-  for i in range(M-K):
-    heappush(h,tmp[K+i])
-  for i in range(K):
-    heappush(h2,(-tmp[i][0],tmp[i][1]))
+  for i in range(M-K): heappush(h,tmp[K+i])
+  for i in range(K): heappush(h2,(-tmp[i][0],tmp[i][1]))
   using=set(tmp[:K])
   ans2=[]
   ans2.append(ans)
@@ -33,27 +31,23 @@ for __ in range(case_no):
       ans-=A[i]
       using.remove((A[i],i))
       x,y=heappop(h)
-      while y<=i:
-        x,y=heappop(h)
+      while y<=i: x,y=heappop(h)
       ans+=x
       using.add((x,y))
+      heappush(h2,(-x,y))
     else:
-      if len(h)>0:
-        x,y=heappop(h)
-        while y<=i and len(h)>0:
-          x,y=heappop(h)
-        if y<=i: continue
-        z,w=heappop(h2)
-        while w<=i:
-          z,w=heappop(h2)
-        if x<-z:
-          ans+=x+z
-          heappush(h2,(-x,y))
-          heappush(h,(-z,w))
-          using.remove((-z,w))
-          using.add((x,y))
-        else:
-          heappush(h,(x,y))
-          heappush(h2,(z,w))
+      x,y=heappop(h)
+      while y<=i: x,y=heappop(h)
+      z,w=heappop(h2)
+      while w<=i: z,w=heappop(h2)
+      if x<-z:
+        ans+=x+z
+        heappush(h,(-z,w))
+        heappush(h2,(-x,y))
+        using.remove((-z,w))
+        using.add((x,y))
+      else:
+        heappush(h,(x,y))
+        heappush(h2,(z,w))
     ans2.append(ans)
   print(*ans2)
--- ABC281_F.py
+++ ABC281_F.py
@@ -3,9 +3,49 @@ import sys
 
 _INPUT = """\
 6
+3
+12 18 11
+10
+0 0 0 0 0 0 0 0 0 0
+5
+324097321 555675086 304655177 991244276 9980291
+2
+1 0
 """
 
 sys.stdin = io.StringIO(_INPUT)
 case_no=int(input())
 for __ in range(case_no):
-  
+  import sys
+  sys.setrecursionlimit(10**6)
+  N=int(input())
+  A=list(map(int,input().split()))
+  A=list(set(A))
+  memo={}
+  def rec(A,n,k):
+    if k in memo: return memo[k]
+    if n==0:
+      if len(A)==2:
+        memo[k]=max(A)
+        return memo[k]
+      if A[0]&1==1:
+        memo[k]=A[0]^1
+        return A[0]^1
+      memo[k]=A[0]
+      return A[0]
+    x,y=[],[]
+    for i in range(len(A)):
+      if (A[i]>>n)&1==1: x.append(A[i])
+      else: y.append(A[i])
+    if len(x)==0:
+      memo[k]=rec(y,n-1,k)
+    elif len(y)==0:
+      for i in range(len(x)):
+        x[i]^=1<<n
+      memo[k]=rec(x,n-1,k^(1<<n))
+    else:
+      for i in range(len(y)):
+        y[i]^=1<<n
+      memo[k]=min(rec(x,n-1,k),rec(y,n-1,k^(1<<n)))
+    return memo[k]
+  print(rec(A,29,1<<30))
